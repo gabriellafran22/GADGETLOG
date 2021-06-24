68b3df93ed7fb8cb7ec9161d7bf095a4a70fa105
gabriellafran22 <gabriellafranchesca22@gmail.com>
2021-06-24 21:50:42 +0700

    kurang yg mode hp ciuuy

--- public/specification.php
+++ public/specification.php
@@ -36,9 +36,10 @@ require "function.php";
     <link rel="stylesheet" href="css/design.css">
     <link rel="stylesheet" href="css/specification.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="plugins/bootstrap.min.css">
-    <script src="plugins/jquery.min.js"></script>
-    <script src="plugins/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
